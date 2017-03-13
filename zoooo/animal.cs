c63537bf049ba2fbeda4e66aa0fbf93757b47f28
Бордунова Анжелика Эдуардовна <aebordunova@edu.hse.ru>
2017-03-13 23:04:32 +0300

    Созданы необходимые классы, начата разработка графического интерфейса

---
+++ zoooo/animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zoooo
+{
+    public class Animal
+    {
+        private int id;
+        private int time4feeding;
+        private int worker;
+        private int section;
+        private int set;
+        private int species;
+    }
+}
---
+++ zoooo/employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zoooo
+{
+    public class Employee
+    {
+        private int name;
+        private int surname;
+        private int workinghours;
+        private int id;
+    }
+}
